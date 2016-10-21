de6aa695ef3c73812ac9560151be0e83ddd52296
Russell Roberts <russrob23@gmail.com>
2016-10-20 19:58:01 -0700

    generate notes controler and added routes for the crud

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,38 @@
+class NotesController < ApplicationController
+	
+	def index
+	end 
+	
+	def show 
+	end
+	
+	def create
+		
+	end
+	
+	def update 
+		
+	end
+
+	def edit		
+	end
+
+	def destroy 
+		
+	end
+
+	def new
+		
+	end
+
+private 
+
+def find
+	
+end
+
+def note_params 
+	
+end
+
+end

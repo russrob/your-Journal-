820c309300748d24942c637e18164dbfc0c7549c
Russell Roberts <russrob23@gmail.com>
2016-10-20 20:59:11 -0700

    added the simple form

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,20 +1,28 @@
 class NotesController < ApplicationController
-	
+	before_action :find_note, only: [:show, :edit, :update, :destroy]  
+
 	def index
+		@notes  = Note.all.order("created_at DESC")
 	end 
 	
 	def show 
 	end
 	
 	def create
-		
+		@note = Note.new(note_params)
+		if @note.save 
+			redirect_to @note 
+		else 
+			render 'new'
+		end 
 	end
 	
 	def update 
 		
 	end
 
-	def edit		
+	def edit
+
 	end
 
 	def destroy 
@@ -22,17 +30,18 @@ class NotesController < ApplicationController
 	end
 
 	def new
-		
+		@note = Note.new 
 	end
 
-private 
+	private 
 
-def find
-	
-end
+	def find_note
+		@note = Note.find(params[:id])
+		  
+	end
 
-def note_params 
-	
-end
+	def note_params 
+		params.require(:note).permit(:title, :content)
+	end
 
 end
